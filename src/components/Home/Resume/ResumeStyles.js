ba203a865f6e88912ec0e2157257fd6d07c5ac04
Munezero Christian <chamsf5@aol.com>
2022-10-14 13:20:47 +0200

    Modified resume modal styles

--- src/components/Home/Resume/ResumeStyles.js
+++ src/components/Home/Resume/ResumeStyles.js
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 
 export default styled.section`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  .modal__background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
   .modal__container {
     width: 900px;
