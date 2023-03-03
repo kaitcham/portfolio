d24dd6e53e452419152c3135b95b9592cb8a88df
Munezero Christian <chamsf5@aol.com>
2023-03-03 14:50:47 +0200

    Add react scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,36 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-scroll';
 import { ImGithub } from 'react-icons/im';
 import { FaLinkedinIn } from 'react-icons/fa';
 import { CgMenu, CgClose } from 'react-icons/cg';
 import Wrapper from './NavbarStyles';
 
 const Navbar = () => {
+  const screenSize = window.innerWidth;
+  const [scroll, setScroll] = useState(0);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  useEffect(() => {
+    window.addEventListener('scroll', () => setScroll(window.scrollY));
+
+    return () => {
+      window.removeEventListener('scroll', () => setScroll(window.scrollY));
+    };
+  }, [scroll]);
+
   return (
-    <Wrapper className="container-fluid">
+    <Wrapper className="container-fluid" scroll={scroll}>
       <div className="container">
-        <Link to="/" className="logo">
+        <Link
+          spy
+          smooth
+          to="home"
+          exact="true"
+          offset={-80}
+          duration={500}
+          className="logo"
+          onClick={() => setIsMenuOpen(false)}
+        >
           <span className="primary-text">M</span>
           <span className="secondary-text">C</span>
           <span>hristian</span>
@@ -19,13 +38,35 @@ const Navbar = () => {
         <nav className="navbar">
           <ul className={isMenuOpen ? 'nav-links show-nav' : 'nav-links'}>
             <li>
-              <Link to="/about">About</Link>
+              <Link
+                spy
+                smooth
+                exact="true"
+                to="about"
+                offset={screenSize < 768 ? -123 : -80}
+                duration={500}
+                activeClass="active"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+              >
+                About
+              </Link>
             </li>
             <li>
-              <a href="/">Portfolio</a>
+              <Link
+                spy
+                smooth
+                exact="true"
+                to="portfolio"
+                offset={screenSize < 768 ? -123 : -80}
+                duration={500}
+                activeClass="active"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+              >
+                Portfolio
+              </Link>
             </li>
             <li>
-              <a href="/">Blogs</a>
+              <Link to="/blogs">Blogs</Link>
             </li>
           </ul>
           <div className="social-icons">
