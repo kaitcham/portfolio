6236733733285c9206844d500cfc6f0ba7199637
kaitcham <chams4f@gmail.com>
2024-02-27 08:14:35 +0200

    remove about section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import HeroPage from '../components/LandingSection';
-import About from '../components/About';
 
 const LandingPage = () => (
   <>
     <Navbar />
     <HeroPage />
-    <About />
   </>
 );
 
