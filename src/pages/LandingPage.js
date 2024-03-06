936168820ef68894b9e36e4e427275150365b048
kaitcham <chamsf5@aol.com>
2024-03-06 18:09:19 +0200

    add contact session

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import HeroPage from '../components/LandingSection';
+import Contact from '../components/Contact';
 
 const LandingPage = () => (
   <>
     <Navbar />
     <HeroPage />
+    <Contact />
   </>
 );
 
