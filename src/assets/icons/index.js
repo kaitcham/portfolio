1298908fd438cb577954b57d71f0f1c6e4883507
kaitcham <chamsf5@aol.com>
2024-03-07 15:18:16 +0200

    add skills icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,8 @@
+export { default } from './JavaScriptIcon';
+export { default as NodeIcon } from './NodeIcon';
+export { default as ReactIcon } from './ReactIcon';
+export { default as NestJSIcon } from './NestJSIcon';
+export { default as GraphqlIcon } from './GraphqlIcon';
+export { default as TailwindIcon } from './TailwindIcon';
+export { default as PostgressIcon } from './PostgressIcon';
+export { default as TypeScriptIcon } from './TypeScriptIcon';
