12604be03671324c133e5e7262d946052b2940fc
kaitcham <chamsf5@aol.com>
2024-03-07 15:22:29 +0200

    add skills section to landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import HeroPage from '../components/LandingSection';
+import Skills from '../components/Skills';
 import Contact from '../components/Contact';
 
 const LandingPage = () => (
   <>
     <Navbar />
     <HeroPage />
+    <Skills />
     <Contact />
   </>
 );
