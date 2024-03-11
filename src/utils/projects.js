9027b68419573f46eef1dc9a1c1d29f35406c305
kaitcham <chamsf5@aol.com>
2024-03-11 16:08:18 +0200

    a list of featured projects

---
+++ src/utils/projects.js
@@ -0,0 +1,63 @@
+export default [
+  {
+    id: 1,
+    name: 'monishare',
+    image:
+      'https://res.cloudinary.com/drapvegx8/image/upload/v1710148011/Monishare_ojo02q.png',
+    description:
+      'A full-stack web application that allows its users to rent moni-cars.',
+    github: '',
+    preview:
+      'http://prod.yellow.monishare.ojemba.s3-website.eu-central-1.amazonaws.com/',
+  },
+
+  {
+    id: 2,
+    name: 'jobster',
+    image:
+      'https://res.cloudinary.com/drapvegx8/image/upload/v1678695004/Projects/Jobster_y5awrs.png',
+    description:
+      'A full-stack web application that allows its users to manage their job applications.',
+    github: 'https://github.com/kaitcham/jobster',
+    preview: 'https://kaitcham-jobster.netlify.app/',
+  },
+
+  {
+    id: 3,
+    name: 'comfy sloth',
+    image:
+      'https://res.cloudinary.com/drapvegx8/image/upload/v1673966781/Projects/comfy_sloth_xh7stz.png',
+    description:
+      'A full-stack web application that allows its users to buy furniture for their homes.',
+    github: 'https://github.com/kaitcham/comfy_sloth',
+    preview: 'https://kaitcham-comfy-sloth.netlify.app',
+  },
+  {
+    id: 4,
+    name: 'laslesVPN',
+    image:
+      'https://res.cloudinary.com/drapvegx8/image/upload/v1710143190/LaslesVPN_g6dnca.png',
+    description: 'A mobile and desktop Figma design to master Tailwind CSS',
+    github: 'https://github.com/kaitcham/laslesVPN',
+    preview: 'https://kaitcham-laslesvpn.netlify.app/',
+  },
+  {
+    id: 5,
+    name: 'ojemba',
+    image:
+      'https://res.cloudinary.com/drapvegx8/image/upload/v1710142960/Ojemba_rskmt0.png',
+    description: 'A mobile and desktop Figma design to master Tailwind CSS.',
+    github: 'https://github.com/kaitcham/ojemba',
+    preview: 'https://kaitcham-ojemba-project.netlify.app/',
+  },
+  {
+    id: 6,
+    name: 'jobs API',
+    image:
+      'https://res.cloudinary.com/drapvegx8/image/upload/v1710160433/JobsAPI_ikplp4.png',
+    description:
+      'An API that allows its users to manage their job applications.',
+    github: 'https://github.com/kaitcham/jobs_api',
+    preview: 'https://jobsapi-l6pm.onrender.com/api-docs/#/',
+  },
+];
