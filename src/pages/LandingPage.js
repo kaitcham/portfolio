68635a0092477536d89252c9f88391519ae331e4
kaitcham <chamsf5@aol.com>
2024-03-11 16:10:03 +0200

    display projects section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar';
 import HeroPage from '../components/LandingSection';
 import Skills from '../components/Skills';
 import Contact from '../components/Contact';
+import Projects from '../components/Projects';
 
 const LandingPage = () => (
   <>
     <Navbar />
     <HeroPage />
     <Skills />
+    <Projects />
     <Contact />
   </>
 );
